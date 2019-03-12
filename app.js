bfe708f37bba2fa4e1d7720c8c5568cd75d6a18c
James <james@jameshawkins.com>
2019-03-11 18:29:32 -0700

    heroku build

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const funds = require('./routes/api/funds');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/public'));
     app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     })
   }
 
